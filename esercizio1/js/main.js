4e69f01bb006d4743b9db71b474276ed2948aa7b
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-09 16:50:52 +0100

    feat: esercizio 1

--- esercizio1/js/main.js
+++ esercizio1/js/main.js
@@ -1,1 +1,26 @@
 "use strict";
+
+// richiedere mail tramite promt e salvare in una variabile
+const mailUtente = prompt("Inserisci la tua mail");
+
+// dichiariamo / assegniamo array di mail
+const listaMail = [
+  "mario.ascione@gmail.com",
+  "davide.rossi@gmail.com",
+  "luca.incerti@gmail.com",
+  "simona.morandi@gmail.com",
+];
+
+// messaggio predefinito
+let messaggio = "- Non puoi accedere";
+
+// ciclo per scorrere le mail
+for (let i = 0; i < listaMail.length; i++) {
+  // condizione per controllare le mail
+  if (listaMail[i] === mailUtente) {
+    messaggio = "- Puoi accedere";
+    break;
+  }
+}
+// stampa in console
+console.log(mailUtente, messaggio);
